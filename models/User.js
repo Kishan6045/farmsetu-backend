7bd50b52c73e51c57d8f47aa53715e941baa2a75
Kishan Thesiya <thesiyakishan546@gmail.com>
2026-01-10 00:53:33 +0530

    Add register login changepassword setup

--- models/User.js
+++ models/User.js
@@ -2,93 +2,93 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const addressSchema = new mongoose.Schema({
-  state: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  district: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  taluko: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  villageName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    state: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    district: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    taluko: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    villageName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
 });
 
 const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: [true, 'First name is required'],
-      trim: true,
+    {
+        firstName: {
+            type: String,
+            required: [true, 'First name is required'],
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: [true, 'Last name is required'],
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+        },
+        phoneNumber: {
+            type: String,
+            required: [true, 'Phone number is required'],
+            unique: true,
+            trim: true,
+        },
+        countryCode: {
+            type: String,
+            required: [true, 'Country code is required'],
+            trim: true,
+            default: '+91',
+        },
+        address: {
+            type: addressSchema,
+            required: [true, 'Address is required'],
+        },
+        pincode: {
+            type: String,
+            required: [true, 'Pincode is required'],
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minlength: [6, 'Password must be at least 6 characters'],
+            select: false, // Don't return password by default
+        },
     },
-    lastName: {
-      type: String,
-      required: [true, 'Last name is required'],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, 'Email is required'],
-      unique: true,
-      lowercase: true,
-      trim: true,
-      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
-    },
-    phoneNumber: {
-      type: String,
-      required: [true, 'Phone number is required'],
-      unique: true,
-      trim: true,
-    },
-    countryCode: {
-      type: String,
-      required: [true, 'Country code is required'],
-      trim: true,
-      default: '+91',
-    },
-    address: {
-      type: addressSchema,
-      required: [true, 'Address is required'],
-    },
-    pincode: {
-      type: String,
-      required: [true, 'Pincode is required'],
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required'],
-      minlength: [6, 'Password must be at least 6 characters'],
-      select: false, // Don't return password by default
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 
 // Hash password before saving
 userSchema.pre('save', async function () {
-  if (!this.isModified('password')) {
-    return;
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+    if (!this.isModified('password')) {
+        return;
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+    return await bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
